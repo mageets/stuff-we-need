80b88a2d04398301ed1efdcef1d85a081c3dcb8f
mageets <mageets@hotmail.com>
2019-08-22 21:57:39 +0100

    created app

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Things we need</div>
+        )
+    }
+}
+
+export default App;
